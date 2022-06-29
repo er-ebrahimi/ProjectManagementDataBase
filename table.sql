bac290f094ac7b4fa6fe1961a1f85c271a115196
er-ebrahimi <erfan.es1381@gmail.com>
2022-06-30 02:32:12 +0430

    add Project ID
    added ProjectID to all the subclasses

--- table.sql
+++ table.sql
@@ -5,9 +5,11 @@ CREATE table account
     lastName NVARCHAR(max) not null,
     phoneNumber CHAR(8),
     Email NVARCHAR(max) NOT NULL,
-    accountID nchar(32) NOT NULL PRIMARY KEY,
-    constraint numberAccount check (ISNUMERIC(phoneNumber) = 1)
+    accountID nchar(32) NOT NULL PRIMARY KEY
 )
+GO
+--ADD constraint
+ALTER TABLE account add constraint numberAccount check (ISNUMERIC(phoneNumber) = 1)
 
 go
 
@@ -17,17 +19,25 @@ create TABLE project
     kind NVARCHAR(max) not null,
     date date NOT NULL,
     time TIME not NULL,
-    discription NVARCHAR(max)
+
 )
+GO
+--Update table
+ALTER TABLE project ADD discription NVARCHAR(max) 
 
 go
 
 CREATE TABLE member
 (
     accountID nchar(32) not NULL,
-    PRIMARY KEY (accountID),
-    constraint FK_member_account FOREIGN KEY(accountID) REFERENCES account(accountID)on delete CASCADE on update CASCADE
+    projectID nchar(32) not NULL,
+    PRIMARY KEY (accountID,projectID),
+    constraint FK_member_account FOREIGN key(accountID) REFERENCES account(accountID)on delete CASCADE on update CASCADE,
+    constraint FK_member_projectID FOREIGN key(projectID) REFERENCES project(projectID)on delete CASCADE on update CASCADE
 )
+GO
+--ADD constraint 
+--alter TABLE member add CONSTRAINT FK_member_account FOREIGN KEY(accountID) REFERENCES account(accountID)on delete CASCADE on update CASCADE
 
 GO
 
@@ -45,8 +55,10 @@ GO
 CREATE TABLE admin
 (
     accountID nchar(32) not NULL,
-    PRIMARY KEY(accountID),
-    constraint FK_admin_account FOREIGN key(accountID) REFERENCES account(accountID)on delete CASCADE on update CASCADE
+    projectID nchar(32) not NULL,
+    PRIMARY KEY(accountID,projectID),
+    constraint FK_admin_account FOREIGN key(accountID) REFERENCES account(accountID)on delete CASCADE on update CASCADE,
+    constraint FK_admin_projectID FOREIGN key(projectID) REFERENCES project(projectID)on delete CASCADE on update CASCADE
 )
 
 go
@@ -55,8 +67,7 @@ CREATE TABLE adminCreateProject
 (
     projectID nchar(32) not null,
     accountID nchar(32),
-    constraint adminCreateProject_project FOREIGN key(projectID) REFERENCES project(projectID) on delete CASCADE on update CASCADE,
-    constraint adminCreateProject_admin FOREIGN key(accountID) REFERENCES admin(accountID) on delete set null on update CASCADE,
+    constraint adminCreateProject_admin FOREIGN key(accountID,projectID) REFERENCES admin(accountID,projectID) on delete CASCADE on update CASCADE,
 )
 
 GO
@@ -64,10 +75,11 @@ GO
 CREATE TABLE observe
 (
     accountID nchar(32) not NULL,
-    PRIMARY KEY(accountID),
-    constraint FK_observe_account FOREIGN key(accountID) REFERENCES account(accountID) on delete CASCADE on update CASCADE
+    projectID nchar(32) not NULL,
+    PRIMARY KEY(accountID,projectID),
+    constraint FK_observe_account FOREIGN key(accountID) REFERENCES account(accountID) on delete CASCADE on update CASCADE ,
+    constraint FK_observe_projectID FOREIGN key(projectID) REFERENCES project(projectID) on delete CASCADE on update CASCADE
 )
-
 GO
 
 CREATE table List
@@ -92,13 +104,21 @@ CREATE TABLE task
     taskID nchar(32) PRIMARY key,
     accountIDCreated nchar(32),--I didn't set (not null) for it becuase if we delete it then the task shouldn't delete
     accountIDMember nchar(32),
-    constraint FK_task_admin FOREIGN key (accountIDCreated) REFERENCES admin(accountID),
-    constraint FK_task_List FOREIGN key (ListID, projectID) REFERENCES List(ListID, projectID) ,
-    constraint dateTask check (startDate < endDate)
+    constraint FK_task_admin FOREIGN key (accountIDCreated,projectID) REFERENCES admin(accountID,projectID),
+    constraint FK_task_List FOREIGN key (ListID, projectID) REFERENCES List(ListID, projectID)
+    ,
 )
+
 GO
+--UPDATE table
+ALTER TABLE task drop COLUMN flag
 
+go
+--update table
+ALTER TABLE task add flag BIT
 
+GO
+--delete update
 ALTER TABLE task DROP constraint FK_task_List;
 
 GO
@@ -106,26 +126,39 @@ GO
 
 ALTER TABLE task add constraint FK_task_List FOREIGN key (ListID, projectID) REFERENCES List(ListID, projectID)   on DELETE CASCADE on update CASCADE
 
--- GO
+GO
+--ADD constraint 
+ALTER TABLE task ADD CONSTRAINT dateTask CHECK (startDate < endDate)
+
+GO
 
 CREATE TABLE admins_task
 (
     accountID nchar(32) not NULL,
+    projectID nchar(32) not NULL,
     taskID nchar(32) not NULL,
-    constraint FK_admins_task_member FOREIGN key (accountID) REFERENCES member(accountID) on DELETE CASCADE on update CASCADE,
+    constraint FK_admins_task_member FOREIGN key (accountID,projectID) REFERENCES member(accountID,projectID),
     constraint FK_member_account_task FOREIGN KEY (taskID) REFERENCES task(taskID) on DELETE CASCADE on update CASCADE
 )
 
+GO
+--delete, update
+--ALTER table admins_task DROP CONSTRAINT FK_admins_task_member
 
+GO
+
+--ALTER TABLE admins_task ADD constraint FK_admins_task_member FOREIGN key (accountID) REFERENCES member(accountID) on DELETE CASCADE on update CASCADE
 
 go
+
 
 CREATE TABLE complete
 (
     complete bit,
     accountID nchar(32) not NULL,
+    projectID nchar(32) not NULL,
     taskID nchar(32) not NULL,
-    constraint FK_complete_member  FOREIGN key (accountID) REFERENCES member(accountID) on DELETE CASCADE on update CASCADE,
+    constraint FK_complete_member  FOREIGN key (accountID,projectID) REFERENCES member(accountID,projectID),
     constraint FK_complete_task FOREIGN KEY (taskID) REFERENCES task(taskID) on DELETE CASCADE on update CASCADE
 )
 
@@ -136,10 +169,11 @@ CREATE TABLE comment
     content NVARCHAR(max),
     data NVARCHAR(max),
     accountID nchar(32),
+    projectID nchar(32),
     taskID nchar(32) not NULL,
     PRIMARY key( taskID),
     constraint FK_comment_task FOREIGN key(taskID) REFERENCES task(taskID) on delete CASCADE on update CASCADE,
-    constraint FK_cmment_account FOREIGN key(accountID) REFERENCES account(accountID) on delete set null on update set NULL
+    constraint FK_cmment_account FOREIGN key(accountID,projectID) REFERENCES member(accountID,projectID)
 )
 
 go
@@ -148,15 +182,14 @@ create table watch
 (
     accountID nchar(32) not null,
     projectID nchar(32) not null,
-    constraint FK_watch_observe FOREIGN key (accountID) REFERENCES observe(accountID) on delete CASCADE on update CASCADE,
-    constraint FK_watch_project FOREIGN key (projectID) REFERENCES project(projectID) on delete CASCADE on update CASCADE
+    constraint FK_watch_observe FOREIGN key (accountID,projectID) REFERENCES observe(accountID,projectID) on delete CASCADE on update CASCADE,
 )
 
 go
 
 create TRIGGER add_admin_member on admin after INSERT as BEGIN
     insert into member
-    select i.accountID
+    select *
     from inserted i
 END
 
@@ -164,7 +197,7 @@ GO
 
 create TRIGGER add_admin_observator on admin after INSERT as BEGIN
     insert into observe
-    select i.accountID
+    select *
     from inserted i
 END
 
@@ -188,7 +221,7 @@ GO
 
 create TRIGGER add_member_observator on member after INSERT as BEGIN
     insert into observe
-    select i.accountID
+    select *
     from inserted i
 END
 
@@ -233,8 +266,14 @@ create TRIGGER delete_task_admin on admin after delete as begin
     where deleted.accountID = task.accountIDCreated
 END
 
-go 
+go
 
+create TRIGGER delete_member_comment on comment after delete as BEGIN
+    UPDATE member
+    set accountID = null
+    from deleted
+    WHERE deleted.accountID = member.accountID and deleted.projectID = member.projectID
+END
 
 
 
