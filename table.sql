e84dbcfb18fd36787c599ca9800203aefe6a2a1b
er-ebrahimi <erfan.es1381@gmail.com>
2022-06-30 17:30:41 +0430

    with cycle
    this edition has cycle error

--- table.sql
+++ table.sql
@@ -3,7 +3,7 @@ CREATE table account
 (
     firstName NVARCHAR(max) not NULL,
     lastName NVARCHAR(max) not null,
-    phoneNumber CHAR(8),
+    phoneNumber CHAR(10),
     Email NVARCHAR(max) NOT NULL,
     accountID nchar(32) NOT NULL PRIMARY KEY
 )
@@ -19,9 +19,10 @@ create TABLE project
     kind NVARCHAR(max) not null,
     date date NOT NULL,
     time TIME not NULL,
-
+    --discription
 )
 GO
+
 --Update table
 ALTER TABLE project ADD discription NVARCHAR(max) 
 
@@ -61,14 +62,14 @@ CREATE TABLE admin
     constraint FK_admin_projectID FOREIGN key(projectID) REFERENCES project(projectID)on delete CASCADE on update CASCADE
 )
 
-go
+-- go
 
-CREATE TABLE adminCreateProject
-(
-    projectID nchar(32) not null,
-    accountID nchar(32),
-    constraint adminCreateProject_admin FOREIGN key(accountID,projectID) REFERENCES admin(accountID,projectID) on delete CASCADE on update CASCADE,
-)
+-- CREATE TABLE adminCreateProject
+-- (
+--     projectID nchar(32) not null,
+--     accountID nchar(32),
+--     constraint adminCreateProject_admin FOREIGN key(accountID,projectID) REFERENCES admin(accountID,projectID) on delete CASCADE on update CASCADE,
+-- )
 
 GO
 
@@ -103,7 +104,6 @@ CREATE TABLE task
     projectID nchar(32),
     taskID nchar(32) PRIMARY key,
     accountIDCreated nchar(32),--I didn't set (not null) for it becuase if we delete it then the task shouldn't delete
-    accountIDMember nchar(32),
     constraint FK_task_admin FOREIGN key (accountIDCreated,projectID) REFERENCES admin(accountID,projectID),
     constraint FK_task_List FOREIGN key (ListID, projectID) REFERENCES List(ListID, projectID)
     ,
@@ -137,7 +137,7 @@ CREATE TABLE admins_task
     accountID nchar(32) not NULL,
     projectID nchar(32) not NULL,
     taskID nchar(32) not NULL,
-    constraint FK_admins_task_member FOREIGN key (accountID,projectID) REFERENCES member(accountID,projectID),
+    constraint FK_admins_task_member FOREIGN key (accountID,projectID) REFERENCES admin(accountID,projectID),
     constraint FK_member_account_task FOREIGN KEY (taskID) REFERENCES task(taskID) on DELETE CASCADE on update CASCADE
 )
 
@@ -159,31 +159,33 @@ CREATE TABLE complete
     projectID nchar(32) not NULL,
     taskID nchar(32) not NULL,
     constraint FK_complete_member  FOREIGN key (accountID,projectID) REFERENCES member(accountID,projectID),
-    constraint FK_complete_task FOREIGN KEY (taskID) REFERENCES task(taskID) on DELETE CASCADE on update CASCADE
+    constraint FK_complete_task FOREIGN KEY (taskID) REFERENCES task(taskID)  on DELETE CASCADE on update CASCADE
 )
+GO
+--delete, update
 
 GO
 
 CREATE TABLE comment
 (
     content NVARCHAR(max),
-    data NVARCHAR(max),
     accountID nchar(32),
     projectID nchar(32),
     taskID nchar(32) not NULL,
-    PRIMARY key( taskID),
+    commentID int not NULL IDENTITY(1,1),
+    PRIMARY key( taskID,commentID),
     constraint FK_comment_task FOREIGN key(taskID) REFERENCES task(taskID) on delete CASCADE on update CASCADE,
     constraint FK_cmment_account FOREIGN key(accountID,projectID) REFERENCES member(accountID,projectID)
 )
 
-go
+-- go
 
-create table watch
-(
-    accountID nchar(32) not null,
-    projectID nchar(32) not null,
-    constraint FK_watch_observe FOREIGN key (accountID,projectID) REFERENCES observe(accountID,projectID) on delete CASCADE on update CASCADE,
-)
+-- create table watch
+-- (
+--     accountID nchar(32) not null,
+--     projectID nchar(32) not null,
+--     constraint FK_watch_observe FOREIGN key (accountID,projectID) REFERENCES observe(accountID,projectID) on delete CASCADE on update CASCADE,
+-- )
 
 go
 
@@ -193,13 +195,6 @@ create TRIGGER add_admin_member on admin after INSERT as BEGIN
     from inserted i
 END
 
-GO
-
-create TRIGGER add_admin_observator on admin after INSERT as BEGIN
-    insert into observe
-    select *
-    from inserted i
-END
 
 GO
 
@@ -237,23 +232,23 @@ go
 
 create TRIGGER delete_project_admin on project after DELETE as BEGIN--TODO maybe we have probleme in this becuase admin delete member to 
     DELETE from admin
-    from deleted, adminCreateProject a
-    where deleted.ProjectID = a.projectID AND a.accountID = admin.accountID
+    from deleted
+    where deleted.ProjectID  = admin.ProjectID
 END
 
 go
 
 create TRIGGER delete_project_member on project after DELETE as BEGIN
     DELETE from member
-    from deleted, adminCreateProject a
-    where deleted.ProjectID = a.projectID AND a.accountID = member.accountID
+    from deleted
+    where deleted.ProjectID = member.ProjectID
 end 
 go
 
 create TRIGGER delete_project_observe on project after DELETE as BEGIN
     DELETE from observe
-    from deleted, adminCreateProject a
-    where deleted.ProjectID = a.projectID AND a.accountID = observe.accountID
+    from deleted
+    where deleted.ProjectID =  observe.ProjectID
 END
 
 GO
@@ -267,23 +262,150 @@ create TRIGGER delete_task_admin on admin after delete as begin
 END
 
 go
-
-create TRIGGER delete_member_comment on comment after delete as BEGIN
-    UPDATE member
+--TODO change alter
+create TRIGGER delete_member_comment on complete after  delete as BEGIN
+    UPDATE comment
     set accountID = null
     from deleted
-    WHERE deleted.accountID = member.accountID and deleted.projectID = member.projectID
+    WHERE deleted.accountID = comment.accountID and deleted.projectID = comment.projectID
 END
+GO
+
+CREATE TRIGGER delete_member_complete on member after delete as BEGIN
+    delete from complete
+    from deleted
+    WHERE deleted.accountID = complete.accountID and deleted.projectID = complete.projectID
+end
+
+GO
+
+INSERT INTO account
+VALUES
+    ('Hossein', 'Rahmani', '0912999999', 'h_rahmani@iust.ac.ir' , 'h_rahmani'),
+    ('Shahrzad', 'Azari Rad', '0912999999', 'shahrzad_azari@comp.iust.ac.ir', 'shahrzad_azari'),
+    ('Arshia', 'Arian Nejad', '0912999999', 'a_ariannejad@comp.iust.ac.ir', 'a_ariannejad'),
+    ('Zahra', 'Amiri' , '0912999999', 'zahra_amiri@comp.iust.ac.ir' , 'zahra_amiri'),
+    ('Alireza', 'Haghani', '0912999999', 'alireza_haqani@gmail.com', 'alireza_haqani'),
+    ('Hanie', 'Jafari' , '0912999999', 'hanie_jafari@comp.iust.ac.ir', 'hanie_jafari'),
+    ('Sadegh', 'Jafari', '0912999999' , 'sadegh_jafari@gmail.com', 'sadegh_jafari'),
+    ('Babak' , 'Behkam Kia' , '0912999999', 'babak_behkamkia@comp.iust.ac.ir', 'babak_behkamkia'),
+    ('Fateme' , 'Zahra Bakhshande' , '0912999999', 'fatemezahrabakhshande@gmail.com', 'fatemezahrabakhshande'),
+    ('Sajjad', 'Ramezani' , '0912999999' , 'sajjad_ramezani@ind.iust.ac.ir', 'sajjad_ramezani'),
+    ('Arman', 'Heidari' , '0912999999', 'heydari_arman@comp.iust.ac.ir' , 'heydari_arman'),
+    ('Morteza', 'Shahrabi Farahani', '0912999999', 'm_shahrabi@comp.iust.ac.ir', 'm_shahrabi'),
+    ('Saman' , 'Mohammadi Raoof', '0912999999', 'saman_mohammadi@comp.iust.ac.ir', 'saman_mohammadi'),
+    ('Niki', 'Majidi Far' , '0912999999' , 'n_majidifard@comp.iust.ac.ir' , 'n_majidifard'),
+    ('Banafshe', 'Gholinejad', '0912999999', 'b_gholinejad@comp.iust.ac.ir', 'b_gholinejad'),
+    ('Parya' , 'Fasahat' , '0912999999' , 'parya_fasahat@comp.iust.ac.ir' , 'parya_fasahat'),
+    ('Melika', 'Nobakhtian' , '0912999999' , 'm_nobakhtian@comp.iust.ac.ir', 'm_nobakhtian'),
+    ('Ilia' , 'Mirzaei' , '0912999999', 'iliya_mirzaei@comp.iust.ac.ir' , 'iliya_mirzaei'),
+    ('Aryan', 'Abdollahi Sabet Nejad' , '0912999999', 'aryan_abdollahi@comp.iust.ac.ir' , 'aryan_abdollahi'),
+    ('Erfan', 'Ebrahimi Soviz' , '0912999999', 'er_ebrahimi@comp.iust.ac.ir' , 'er_ebrahimi'),
+    ('Amir Ali', 'Farazmand' , '0912999999', 'amirali_farazmand@comp.iust.ac.ir', 'amirali_farazmand')
 
 
 
+GO
+
+INSERT INTO project
+VALUES
+    ('Database Course', 'Course' , '20220301', '13:30' , 'Darse Database Daneshkade Computer' ),
+    ('Project Management Database' , 'Agile' , GETDATE() , '13:30', 'Project modiriat proje')
+
+GO
+
+INSERT INTO [admin]
+VALUES
+    ('h_rahmani', 'Database Course'  ),
+    ('m_shahrabi', 'Database Course' ),
+    ('er_ebrahimi', 'Project Management Database')
+
+GO
+
+INSERT INTO member
+VALUES
+    ('shahrzad_azari', 'Database Course'),
+    ('a_ariannejad' , 'Database Course'),
+    ('zahra_amiri' , 'Database Course'),
+    ('alireza_haqani' , 'Database Course'),
+    ('hanie_jafari', 'Database Course'),
+    ('sadegh_jafari', 'Database Course'),
+    ('babak_behkamkia', 'Database Course'),
+    ('fatemezahrabakhshande', 'Database Course'),
+    ('sajjad_ramezani', 'Database Course' ),
+    ('heydari_arman', 'Database Course'),
+    ('saman_mohammadi', 'Database Course'),
+    ('n_majidifard', 'Database Course'),
+    ('b_gholinejad', 'Database Course'),
+    ('parya_fasahat' , 'Database Course'),
+    ('m_nobakhtian', 'Database Course' ),
+    ('iliya_mirzaei', 'Database Course' ),
+    ('amirali_farazmand', 'Project Management Database' ),
+    ('aryan_abdollahi', 'Project Management Database' )
+
+GO
 
 
 
+GO
+
+INSERT INTO accountHasProject
+VALUES
+    ('shahrzad_azari', 'Database Course'),
+    ('a_ariannejad' , 'Database Course'),
+    ('zahra_amiri' , 'Database Course'),
+    ('alireza_haqani' , 'Database Course'),
+    ('hanie_jafari', 'Database Course'),
+    ('sadegh_jafari', 'Database Course'),
+    ('babak_behkamkia', 'Database Course'),
+    ('fatemezahrabakhshande', 'Database Course'),
+    ('sajjad_ramezani' , 'Database Course'),
+    ('heydari_arman', 'Database Course'),
+    ('saman_mohammadi' , 'Database Course'),
+    ('n_majidifard', 'Database Course'),
+    ('b_gholinejad', 'Database Course'),
+    ('parya_fasahat' , 'Database Course'),
+    ('m_nobakhtian', 'Database Course'),
+    ('iliya_mirzaei' , 'Database Course'),
+    ('amirali_farazmand', 'Project Management Database'),
+    ('aryan_abdollahi', 'Project Management Database'),
+    ('h_rahmani', 'Database Course'  ),
+    ('m_shahrabi', 'Database Course' ),
+    ('er_ebrahimi', 'Project Management Database')
 
 
 
+GO
+
+INSERT into List
+VALUES
+    ('quiz' , 'Database Course')
+
+GO
+
+insert into task
+VALUES
+    ( '20220610', '20220710' , 'quiz in middle of term', 'check', 'quiz', 'Database Course', 'quiz1' , 'm_shahrabi', 0  )
+GO
+
+--TODO how create project should added to admins_task
+insert into admins_task
+VALUES
+    ('m_shahrabi' , 'Database Course', 'quiz1' ),
+    ('h_rahmani' , 'Database Course', 'quiz1')
+
+GO
+
+insert into complete
+VALUES
+    (1, 'sadegh_jafari', 'Database Course' , 'quiz1' )
+
+go
+
+INSERT into comment
+VALUEs
+    ('hello', 'sadegh_jafari', 'Database Course' , 'quiz1' )
 
 
-
-
+-- delete from comment where commentID = 1
+delete from member where accountID = 'sadegh_jafari'
